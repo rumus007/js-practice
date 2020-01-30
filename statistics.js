835deac9f8a56e909dd899a7a9dfdba580e7f887
Rumus Bajracharya <rumus.bajracharya@yipl.com.np>
2020-01-30 11:31:17 +0545

    Initial commit

---
+++ statistics.js
@@ -0,0 +1,29 @@
+var numbers = [3, 5, 4, 4, 1, 1, 2, 3];
+var toPrint = mode(numbers);
+console.log('The modes are >> ', toPrint);
+
+
+
+function mode(numbers) {
+    // as result can be bimodal or multi-modal,
+    // the returned result is provided as an array
+    // mode of [3, 5, 4, 4, 1, 1, 2, 3] = [1, 3, 4]
+    var modes = [], count = [], i, number, maxIndex = 0;
+ 
+    for (i = 0; i < numbers.length; i += 1) {
+        number = numbers[i];
+        count[number] = (count[number] || 0) + 1;
+        if (count[number] > maxIndex) {
+            maxIndex = count[number];
+        }
+    }
+ 
+    for (i in count)
+        if (count.hasOwnProperty(i)) {
+            if (count[i] === maxIndex) {
+                modes.push(Number(i));
+            }
+        }
+ 
+    return modes;
+}
