5ead9889faff1d20591d85c2e24d47700674b756
Rumus Bajracharya <rumus.bajracharya@yipl.com.np>
2020-02-20 13:06:56 +0545

    Node server created

---
+++ server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+const PORT = 3000;
+
+
+const getHtml = (path) => {
+
+    return new Promise((resolve,reject) => {
+        // Filename
+        const fileName = __dirname+'/'+path;
+        console.log(fileName);
+        // Read 
+        fs.readFile
+        fs.readFile( fileName, (err, data) => {
+          if (err) {
+              reject(err)
+          };
+          
+          resolve(data);
+        });
+    });
+}
+
+
+const server = http.createServer(function(request,response) {
+    getHtml('index.html')
+        .then((data) => {    
+            response.writeHead(200,{"Content-Type": "text/html"});
+            response.write(data);
+            response.end();
+        })
+        .catch(err => {
+            response.writeHead(404,{"Content-Type": "text/html"});
+            response.write("NOT FOUND");
+            response.end();
+        })
+
+});
+
+
+server.listen(PORT, console.log("server started"));
